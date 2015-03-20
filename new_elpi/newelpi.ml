c080576023c1778b29ce59fd962e2488651b9d1c
Tsvetan Dunchev <cdunchev@hotmail.com>
2015-03-20 19:46:44 +0100

    New option -impl n to select the n-th implementation

--- new_elpi/newelpi.ml
+++ new_elpi/newelpi.ml
@@ -20,8 +20,8 @@ let test_prog prog query =
     else prerr_endline "success\n") implementations
 ;;
 
-let run_prog prog query =
- let msg,_,main_loop = List.hd implementations in
+let run_prog impl prog query =
+ let msg,_,main_loop = List.nth implementations (impl-1) in
  prerr_endline (msg query);
  main_loop prog query
 ;;
@@ -29,7 +29,10 @@ let run_prog prog query =
 let _ =
   let argv = Sys.argv in
   (* j=1 iff -test is not passed *)
-  let j = if argv.(1) = "-test" then 2 else 1 in
+  let j,test,impl =
+   if argv.(1) = "-test" then 2,true,0
+   else if argv.(1) = "-impl" then 3,false,int_of_string (argv.(2))
+   else 1,false,1 in
   let b = Buffer.create 1024 in
   for i=j to Array.length argv - 1 do
     Printf.eprintf "loading %s\n" argv.(i);
@@ -42,10 +45,10 @@ let _ =
   let p = Buffer.contents b in
   let p = Parser.parse_program p in
   let g =
-   if j = 1 then (
+   if not test then (
     Printf.printf "goal> %!";
     input_line stdin)
    else "main." in
   let g = Parser.parse_goal g in
-  if j=1 then run_prog p g else test_prog p g
+  if test then test_prog p g else run_prog impl p g
 ;;
