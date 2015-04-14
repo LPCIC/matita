9f7a1dde1811ebc7e3cafc950b886a4e8941e50f
Cvetan Dunchev <cdunchev@hotmail.com>
2015-04-14 14:48:23 +0200

    lprun.ml removed + new test
    Rationale:
    1. lprun.ml is less general than lprun2.ml and it runs even slightly
       slower now (??)
    2. the new test shows that derefs are missing and thus ho goals fail.

--- new_elpi/newelpi.ml
+++ new_elpi/newelpi.ml
@@ -1,4 +1,4 @@
-let implementations = Lprun.implementations @ Lprun2.implementations @ Lprun3.implementations @ Lprun4.implementations
+let implementations = Lprun2.implementations @ Lprun3.implementations @ Lprun4.implementations
 
 (*
 let _ =
