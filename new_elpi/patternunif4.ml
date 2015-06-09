252ddf8d37d7e34a44bdb4ee87c5acaeebccd314
Claudio Sacerdoti Coen <sacerdot@cuda.cs.unibo.it>
2015-06-09 16:35:06 +0200

    Semantics of $lt modified/fixed.
    Now ($lt c1 c2) holds iff c1 is older than c2.
    
    NOTE: untested.

--- new_elpi/patternunif4.ml
+++ new_elpi/patternunif4.ml
@@ -635,7 +635,10 @@ let _ =
       | _ -> assert false in
     match args with
        [t1; t2] ->
-         if get_constant t1 <= get_constant t2 then raise (Failure "not lt")
+         let t1 = get_constant t1 in
+         let t2 = get_constant t2 in
+         let is_lt = if t1 <= 0 then t2 > 0 || t2 < t1 else t1 < t2 in
+         if not is_lt then raise (Failure "not lt")
      | _ -> assert false)
 ;;
 
